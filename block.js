e3b21c68fca78e0d0242781576036cceb03f2615
jhope-q30 <jhope@q30design.com>
2021-06-22 15:25:42 -0400

    init

---
+++ block.js
@@ -0,0 +1,164 @@
+( function( blocks, blockEditor, i18n, element, components, _ ) {
+
+	var el               = element.createElement;
+	var PlainText        = blockEditor.PlainText;
+	var RichText         = blockEditor.RichText;
+	var MediaUpload      = blockEditor.MediaUpload;
+	var RadioControl     = components.RadioControl;
+
+	var colorOptions     = 5;
+
+	blocks.registerBlockType( 'q30-hero-block/q30-hero-style', {
+		title: i18n.__( 'hero layout style', 'q30-hero-block' ),
+		icon: 'index-card',
+		category: 'layout',
+		attributes: {
+			mediaID: {
+				type:      'number',
+			},
+			mediaURL: {
+				type:      'string',
+				source:    'attribute',
+				selector:  'img',
+				attribute: 'src',
+			},
+			mediaALT: {
+				type:      'string',
+				source:    'attribute',
+				selector:  'img',
+				attribute: 'alt',
+				default:   '',
+			},
+			heroCOLOR: {
+				type:      'string',
+				source:    'attribute',
+				selector:  'div',
+				attribute: 'color',
+				default:   'c-0',
+			},
+			heroprefix: {
+				type:      'array',
+				source:    'children',
+				selector:  '.hero-prefix',
+			},
+			herotitle: {
+				type:      'array',
+				source:    'children',
+				selector:  '.hero-title',
+			},
+			herotext: {
+				type:      'array',
+				source:    'children',
+				selector:  '.hero-text',
+			},
+		},
+		edit: function( props ) {
+			
+			var attributes = props.attributes;
+
+			var onSelectImage = function( media ) {
+				return props.setAttributes( {
+					mediaURL: media.url,
+					mediaID:  media.id,
+					mediaALT: media.alt,
+				} );
+			};
+			var createColorOptions = function(){
+				var ref = [];
+				for( var q = 0; q < colorOptions; ++q ){
+					ref.push( { label: String( 'option: ' + ( q + 1 ) ), value: String( 'c-' + q ) } );
+				}
+				return ref;
+			}
+
+			return (
+				el( 'div', { className: props.className },
+					el( 'div', { className: 'hero-image' },
+						el( MediaUpload, {
+							onSelect: onSelectImage,
+							allowedTypes: 'image',
+							value: attributes.mediaID,
+							render: function( obj ) {
+								return el( components.Button, {
+										className: attributes.mediaID ? 'image-button' : 'button button-large',
+										onClick: obj.open
+									},
+									! attributes.mediaID ? i18n.__( 'Upload Image', 'q30-hero-block' ) : el( 'img', { src: attributes.mediaURL, alt: attributes.mediaALT } )
+								);
+							}
+						} )
+					),
+					el( RadioControl, {
+						className: 'hero-colors',
+						label: 'Hero color options',
+						help: 'Choose the hero color background for title and text',
+						selected: attributes.heroCOLOR,
+						options: createColorOptions(),
+						onChange: function( value ) {
+							props.setAttributes( { heroCOLOR: value } );
+						},
+					} ),
+					el( 'div', { className: 'hero-title-content' },
+						el( PlainText, {
+							value: attributes.heroprefix,
+							onChange: function( value ) {
+								props.setAttributes( { heroprefix: value } );
+							},
+						} ),
+						el( 'span', { className: 'hero-prefix-space' }, ':' ),
+						el( PlainText, {
+							value: attributes.herotitle,
+							onChange: function( value ) {
+								props.setAttributes( { herotitle: value } );
+							},
+						} ),
+					),
+					el( RichText, {
+						tagName: 'p',
+						placeholder: i18n.__( 'Add hero text', 'q30-hero-block' ),
+						formattingControls: [ 'bold', 'italic', 'link' ],
+						isSelected: true,
+						value: attributes.herotext,
+						onChange: function( value ) {
+							props.setAttributes( { herotext: value } );
+						},
+					} )
+				)
+			);
+		},
+		save: function( props ) {
+
+			var attributes   = props.attributes;
+			var prefix_space = attributes.heroprefix != '' ? el( 'span', { className: 'hero-prefix-space', }, ':' ) : '';
+
+			return (
+				el( 'div', { className: props.className, color: attributes.heroCOLOR },
+					attributes.mediaURL &&
+					el( 'div', { className: 'hero-image' },
+						el( 'img', { src: attributes.mediaURL, alt: attributes.mediaALT } ),
+						el( 'div', { className: 'hero-content' },
+							el( 'div', { className: attributes.heroCOLOR },
+								el( 'div', { className: 'hero-title-content' },
+									el( 'span', { className: 'hero-prefix' }, attributes.heroprefix ),
+									prefix_space,
+									el( 'span', { className: 'hero-title' }, attributes.herotitle ),
+								),
+								el( RichText.Content, { tagName: 'p', value: attributes.herotext, className: 'hero-text', } ),
+							),
+						),
+					),
+				)
+			);
+
+		},
+
+	} );
+
+} )(
+	window.wp.blocks,
+	window.wp.blockEditor,
+	window.wp.i18n,
+	window.wp.element,
+	window.wp.components,
+	window._,
+);
---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * Plugin Name: q30 Hero Block
+ * Plugin URI: https://github.com/jhope-q30/q30-hero-block
+ * Description: This adds hero layout/style to any theme q30-hero-style. Styles that can be extended via theme are the parent class ( .wp-block-q30-hero-block-q30-hero-style ), and child classes within ( .hero-image, .hero-image img, .hero-content, .hero-content .hero-text, .hero-content .hero-text p ).
+ * Version: 1.0.0
+ * Author: jhope@q30design.com
+ *
+ * @package q30-hero-block
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Load all translations for our plugin from the MO file.
+*/
+add_action( 'init', 'q30_hb_load_textdomain' );
+
+function q30_hb_load_textdomain() {
+	load_plugin_textdomain( 'q30-hero-block', false, basename( __DIR__ ) . '/languages' );
+}
+
+/**
+ * Registers all block assets so that they can be enqueued through Gutenberg in
+ * the corresponding context.
+ *
+ * Passes translations to JavaScript.
+ */
+function q30_hb_register_block() {
+
+	if ( ! function_exists( 'register_block_type' ) ) {
+		// Gutenberg is not active.
+		return;
+	}
+
+	wp_register_script(
+		'q30-hero-block-00',
+		plugins_url( 'block.js', __FILE__ ),
+		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor', 'underscore' ),
+		filemtime( plugin_dir_path( __FILE__ ) . 'block.js' )
+	);
+
+	wp_register_style(
+		'q30-hero-block-00-editor',
+		plugins_url( 'editor.css', __FILE__ ),
+		array( 'wp-edit-blocks' ),
+		filemtime( plugin_dir_path( __FILE__ ) . 'editor.css' )
+	);
+
+	wp_register_style(
+		'q30-hero-block-00',
+		plugins_url( 'style.css', __FILE__ ),
+		array( ),
+		filemtime( plugin_dir_path( __FILE__ ) . 'style.css' )
+	);
+
+	register_block_type( 'q30-hero-block/q30-hero-style', array(
+		'style'         => 'q30-hero-block-00',
+		'editor_style'  => 'q30-hero-block-00-editor',
+		'editor_script' => 'q30-hero-block-00',
+	) );
+
+  if ( function_exists( 'wp_set_script_translations' ) ) {
+    /**
+     * May be extended to wp_set_script_translations( 'my-handle', 'my-domain',
+     * plugin_dir_path( MY_PLUGIN ) . 'languages' ) ). For details see
+     * https://make.wordpress.org/core/2018/11/09/new-javascript-i18n-support-in-wordpress/
+     */
+    wp_set_script_translations( 'q30-hero-block-00', 'q30-hero-block' );
+  }
+
+}
+add_action( 'init', 'q30_hb_register_block' );
